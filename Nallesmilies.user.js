bf6ab86301812bfe33f5d7a49fd93e726de1a17d
Björn Mårtensson <bearmarshal@gmail.com>
2016-02-16 18:58:43 +0100

    1.1.0: Made URI storage local for greater platform independence.

--- Nallesmilies.user.js
+++ Nallesmilies.user.js
@@ -3,29 +3,27 @@
 // @description This script replaces all smilies in the smilie box on the GiantitP forums with custom nalle counterparts.
 // @icon http://i.imgur.com/jLvmsd9.png
 // @namespace http://www.bearmarshal.com/scripts/greasemonkey
-// @version 1.0.1
+// @version 1.1.0
 // @match *://*.giantitp.com/forums/*
 // ==/UserScript==
 
-if (!GM_getValue("nallesmilies.initialised", false)) {
-	GM_setValue("nallesmilies.smallamused.img", "http://i.imgur.com/rcxULF7.png");
-	GM_setValue("nallesmilies.smallannoyed.img", "http://i.imgur.com/naZauqK.png");
-	GM_setValue("nallesmilies.smallbiggrin.img", "http://i.imgur.com/haudcUB.png");
-	GM_setValue("nallesmilies.smallconfused.img", "http://i.imgur.com/kavzNLd.png");
-	GM_setValue("nallesmilies.smalleek.img", "http://i.imgur.com/AT4x3dQ.png");
-	GM_setValue("nallesmilies.smallfrown.img", "http://i.imgur.com/xVZ2F1e.png");
-	GM_setValue("nallesmilies.smallfurious.img", "http://i.imgur.com/aPNEHAS.png");
-	GM_setValue("nallesmilies.smallmad.img", "http://i.imgur.com/RdylcEy.png");
-	GM_setValue("nallesmilies.smallredface.img", "http://i.imgur.com/gh2zaGF.png");
-	GM_setValue("nallesmilies.smallsigh.img", "http://i.imgur.com/b6MRqPf.png");
-	GM_setValue("nallesmilies.smallsmile.img", "http://i.imgur.com/jLvmsd9.png");
-	GM_setValue("nallesmilies.smalltongue.img", "http://i.imgur.com/FZ6yI9l.png");
-	GM_setValue("nallesmilies.smallwink.img", "http://i.imgur.com/ZRHtTJs.png");
-	GM_setValue("nallesmilies.smallyuk.img", "http://i.imgur.com/3VDrLoW.png");
-	GM_setValue("nallesmilies.smallcool.img", "TODO: create a :smallnallecool:");
-	
-	GM_setValue("nallesmilies.initialised", true);
-}
+var nallesmilies = {
+	smallamused : "http://i.imgur.com/rcxULF7.png",
+	smallannoyed : "http://i.imgur.com/naZauqK.png",
+	smallbiggrin : "http://i.imgur.com/haudcUB.png",
+	smallconfused : "http://i.imgur.com/kavzNLd.png",
+	smalleek : "http://i.imgur.com/AT4x3dQ.png",
+	smallfrown : "http://i.imgur.com/xVZ2F1e.png",
+	smallfurious : "http://i.imgur.com/aPNEHAS.png",
+	smallmad : "http://i.imgur.com/RdylcEy.png",
+	smallredface : "http://i.imgur.com/gh2zaGF.png",
+	smallsigh : "http://i.imgur.com/b6MRqPf.png",
+	smallsmile : "http://i.imgur.com/jLvmsd9.png",
+	smalltongue : "http://i.imgur.com/FZ6yI9l.png",
+	smallwink : "http://i.imgur.com/ZRHtTJs.png",
+	smallyuk : "http://i.imgur.com/3VDrLoW.png",
+	smallcool : "TODO: create a :smallnallecool:"
+};
 
 let smilieBox = document.getElementById("vB_Editor_001_smiliebox");
 
@@ -36,15 +34,15 @@ if (smilieBox == null) {
 let smilies = smilieBox.children;    
 for (let i = 0; i < smilies.length; i++) {
   let img = smilies[i].firstElementChild.firstElementChild.firstElementChild;
-  let type = /:(\w+):/.exec(img.alt);
-  if (type[1] == "smallcool") {
+  let smilieType = /:(\w+):/.exec(img.alt);
+  if (smilieType[1] == "smallcool") {
 	img.alt = "[roll]1d20[/roll]";
 	img.src = "http://www.giantitp.com/forums/images/sand/icons/icon_d20.png";
 	img.title = "Roll 1d20"
   } else {
-	let nalle = GM_getValue("nallesmilies." + type[1] + ".img");
-	img.alt = "[IMG]" + nalle + "[/IMG]";
-	img.src = nalle;
+	let nalleURI = nallesmilies[smilieType[1]];
+	img.alt = "[IMG]" + nalleURI + "[/IMG]";
+	img.src = nalleURI;
   }
   
 }
